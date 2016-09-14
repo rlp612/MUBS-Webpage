6da20a6e69c25786d2fcc498524a2a82c6c8bd52
r.zach.lamberty <r.zach.lamberty@gmail.com>
2016-09-13 23:30:11 -0400

    moved form into tab and fixed boolean field types

--- FlaskWebProject/forms.py
+++ FlaskWebProject/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import SubmitField
+from wtforms import BooleanField, SubmitField
 from wtforms_sqlalchemy.orm import model_form
 
 from . import db
@@ -9,4 +9,19 @@ from .models import CommunityAttributes
 CommunityAttributesForm = model_form(
     CommunityAttributes, db_session=db.session, base_class=Form
 )
+
+# tinyint = boolean, thanks mysql, never change
+fieldOverrides = {
+    'Gated_Access': BooleanField,
+    'On_site_management': BooleanField,
+    'Clubhouse': BooleanField,
+}
+
+for (fieldname, fieldtype) in tinyintfields.items():
+    try:
+        getattr(CommunityAttributesForm, fieldname).field_class = fieldtype
+    except:
+        pass
+
+# finally, a way to submit the form
 CommunityAttributesForm.submit = SubmitField('Update')
